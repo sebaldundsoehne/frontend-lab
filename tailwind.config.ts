752faa4779da9ea05fd678c85e3190ef99d0798f
sebaldundsoehne <kontakt@sebaldundsoehne.de>
2026-02-02 12:24:01 +0100

    add FUNKEpublish font as base font

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['FUNKEpublish', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+} satisfies Config
